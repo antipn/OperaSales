61386dbb6d9b70065657a128cbed68dc23311915
antipn <palmer@bk.ru>
2022-03-13 18:13:35 +0300

    Init app OperaSales: add gitignore file and filed pom.xml

---
+++ src/main/java/ru/learnup/java2/antipn/spring/boot/operasales/operasales/OperaSalesApplication.java
@@ -0,0 +1,13 @@
+package ru.learnup.java2.antipn.spring.boot.operasales.operasales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperaSalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperaSalesApplication.class, args);
+    }
+
+}

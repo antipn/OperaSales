b46efdde09672dce6dbaa6b1f1a2b7342cd61150
antipn <palmer@bk.ru>
2022-03-13 19:16:51 +0300

    Added repo pack and class with interfaces for access data in DB

---
+++ src/main/java/ru/learnup/java2/antipn/spring/boot/operasales/operasales/reporsitories/EventRepository.java
@@ -0,0 +1,16 @@
+package ru.learnup.java2.antipn.spring.boot.operasales.operasales.reporsitories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.learnup.java2.antipn.spring.boot.operasales.operasales.entity.PublicEvent;
+
+
+import java.util.List;
+
+@Repository
+public interface EventRepository extends JpaRepository<PublicEvent, Integer> {
+    List<PublicEvent> getAllByPublicEventNameContains(String nameContains); //по имени нативный запрос Spring
+
+    List<PublicEvent> getAllByRatingRARSContains(String rarsContains); //по рейтингу
+
+}
